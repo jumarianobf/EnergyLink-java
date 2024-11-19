f48d1c34a7ed672a079d539e426a9bb345cbeb37
Julia <jumarianobf@gmail.com>
2024-11-19 20:12:09 -0300

    feat: renomeando nome do test para global solution

--- src/test/java/br/com/fiap/gs/GlobalSolutionApplicationTests.java
+++ src/test/java/br/com/fiap/gs/GlobalSolutionApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChallengeApplicationTests {
+class GlobalSolutionApplicationTests {
 
 	@Test
 	void contextLoads() {
